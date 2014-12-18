5e9b3a6de0bafc2dbae8aa78b81c2e282e3575b4
Raphael Matile <raphael.matile@bluewin.ch>
2014-12-18 20:52:47 +0100

    Add login and logout functionality

--- src/rmatil/cms/Controller/LoginController.php
+++ src/rmatil/cms/Controller/LoginController.php
@@ -4,6 +4,8 @@ namespace rmatil\cms\Controller;
 
 use SlimController\SlimController;
 use rmatil\cms\Constants\HttpStatusCodes;
+use rmatil\cms\Constants\EntityNames;
+use rmatil\cms\Utils\PasswordUtils;
 use Doctrine\ORM\EntityManager;
 
 class LoginController extends SlimController {
@@ -13,12 +15,39 @@ class LoginController extends SlimController {
     }
 
     public function doLoginAction() {
-        var_dump("asdf");
-        exit();
-        $this->app->response->setBody("asdf");
+        $entityManager   = $this->app->entityManager;
+        $userRepository  = $entityManager->getRepository(EntityNames::USER);
+
+        $username = $this->app->request->params('username');
+        $password = $this->app->request->params('password');
+
+        $user = $userRepository->findOneBy(array('userName' => $username));
+
+        if (null === $user) {
+            $this->app->render('login-form.php', array('error' => 'password or username is wrong'));
+            return;
+        }
+
+        if (PasswordUtils::isEqual($password, $user->getPasswordHash())) {
+            $_SESSION['user_id'] = $user->getId();
+            $_SESSION['user_user_name'] = $user->getUserName();
+            $_SESSION['user_first_name'] = $user->getFirstName();
+            $_SESSION['user_last_name'] = $user->getLastName();
+            $_SESSION['user_last_login_date'] = $user->getLastLoginDate();
+            $_SESSION['is_logged_in'] = true;
+            $this->app->redirect('/cms');
+        }
+
+        $this->app->response->setStatus(HttpStatusCodes::FORBIDDEN);
+        $this->app->render('login-form.php', array('error' => 'password or username is wrong'));
     }
 
     public function doLogoutAction() {
+        unset($_SESSION['user']);
+        unset($_SESSION['is_logged_in']);
+        session_destroy();
+        session_write_close();
 
+        $this->app->redirect('/');
     }
 }
