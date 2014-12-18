94ab2050770d823248c568727256f29fb2e3f708
Raphael Matile <raphael.matile@bluewin.ch>
2014-12-18 20:51:01 +0100

    Add login form

--- web/slim-templates/login-form.php
+++ web/slim-templates/login-form.php
@@ -1,4 +1,10 @@
 <h1>Login</h1>
+<?php
+if (isset($error)) {
+    echo '<h4>'.$error.'</h4>';
+}
+
+?>
 <form action="/login/do-login" method="post">
     <input type="text" id="username" name="username" placeholder="Benutzername" />
     <input type="password" id="password" name="password" />
