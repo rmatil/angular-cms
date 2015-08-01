29231d9d8c2d4c661da6b09040591b7cd0c8318b
Raphael Matile <raphael.matile@bluewin.ch>
2015-08-01 16:16:11 +0200

    Add GenericApiService

--- web/cms/components/api/genericApiService.js
+++ web/cms/components/api/genericApiService.js
@@ -1,9 +1,75 @@
 'use strict';
 
-function GenericApiService() {
+function GenericApiService($http, $log) {
 
-    this.getArticles = function () {
-        return [{"title": "article1", "content": "Content 1"}];
+    var apiEndPoint = '/api';
+
+    var printError = function (error) {
+        // TODO: make a generic version of this
+        // TODO: set log level for information to debug in $logProvider
+        $log.error('[' + error.config.method + '] ' + error.status + ' "' + error.statusText + '" "' + error.config.url + '"');
+    };
+
+    this.get = function (objectIdentifier) {
+        return $http.get(apiEndPoint + '/' + objectIdentifier)
+            .then(function (response) {
+                return response.data;
+            }).catch(function (error) {
+                printError(error);
+            });
+    };
+
+    this.getObject = function (objectIdentifier, objectId) {
+        return $http.get(apiEndPoint + '/' + objectIdentifier + '/' + objectId)
+            .then(function (response) {
+                return response.data;
+            }).catch(function (error) {
+                printError(error);
+            });
+    };
+
+    this.getEmptyObject = function (objectIdentifier) {
+        return $http.get(apiEndPoint + '/empty/' + objectIdentifier )
+            .then(function (response) {
+                return response.data;
+            }).catch(function (error) {
+                printError(error);
+            });
+    };
+
+    this.post = function (objectIdentifier, object) {
+        return $http({
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            url: apiEndPoint + '/' + objectIdentifier,
+            method: 'POST',
+            data: object
+        }).then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            printError(error);
+        });
+    };
+
+    this.put = function (objectIdentifier, object) {
+        return $http({
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            url: apiEndPoint + '/' + objectIdentifier + '/' + object.id,
+            method: 'PUT',
+            data: object
+        }).then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            printError(error);
+        });
+    }
+
+    this.remove = function (objectIdentifier, objectId) {
+        return $http.delete(apiEndPoint + '/' + objectIdentifier + '/' + objectId)
+            .then(function (response) {
+                return response.data;
+            }).catch(function (error) {
+                printError(error);
+            });
     }
 
 }
@@ -12,4 +78,6 @@ function GenericApiService() {
     angular
         .module('cms.services')
         .service('GenericApiService', GenericApiService);
+
+        GenericApiService.$inject = [ '$http', '$log'];
 }());
