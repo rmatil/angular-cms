187aa8fea20955ade0236c4e61da1eaeed3b0c2b
Raphael Matile <raphael.matile@gmail.com>
2015-08-21 22:23:34 +0200

    Add page name directive

--- web/cms/components/navigation/navigation.js
+++ web/cms/components/navigation/navigation.js
@@ -16,12 +16,23 @@ angular.module('cms.directives')
             restrict: 'E',
             isolate: true,
             link: function ($scope, $elm, $attrs) {
-                var submenuProperties = NavigationService.getSubnavigation($location.path());
+                var submenuProperties = NavigationService.getSubNavigation($location.path(), true);
                 if (null !== submenuProperties) {
                     $scope.subNavigation = submenuProperties;
                 }
             },
             scope: true,
             templateUrl: "components/navigation/content-navigation.html"
+        };
+    }])
+    .directive('pageName', ['NavigationService', '$location', function (NavigationService, $location) {
+        return {
+            restrict: 'E',
+            isolate: true,
+            link: function ($scope, $elm, $attrs) {
+                $scope.pageName = NavigationService.getPageName($location.path());
+            },
+            scope: true,
+            template: '{{ pageName }}'
         };
     }]);
--- web/cms/components/navigation/navigationService.js
+++ web/cms/components/navigation/navigationService.js
@@ -2,14 +2,15 @@
 
 function NavigationService() {
 
-    var menuArray = {
+    var that = this,
+        menuArray = {
         "dashboard": {
             "backgroundColorClass": "lightyellow",
             "topBorderClass": "lightyellow-top-border",
             "menuArray": [
                 {
-                    "name": "Übersicht",
-                    "link": "\/cms\/dashboard\/"
+                    "name": "Dashboard",
+                    "link": "\/cms\/"
                 }
             ]
         },
@@ -19,19 +20,33 @@ function NavigationService() {
             "menuArray": [
                 {
                     "name": "Article Overview",
-                    "link": "articles\/list\/"
+                    "link": "articles\/list",
+                    "inMenu": true
                 },
                 {
                     "name": "Add Article",
-                    "link": "articles\/add\/"
+                    "link": "articles\/add",
+                    "inMenu": true
+                },
+                {
+                    "name": "Edit Article",
+                    "link": "articles\/article\/:id",
+                    "inMenu": false
                 },
                 {
                     "name": "Page Overview",
-                    "link": "pages\/list\/"
+                    "link": "pages\/list",
+                    "inMenu": true
                 },
                 {
                     "name": "Add Page",
-                    "link": "pages\/add\/"
+                    "link": "pages\/add",
+                    "inMenu": true
+                },
+                {
+                    "name": "Edit Page",
+                    "link": "pages\/page\/:id",
+                    "inMenu": false
                 }
             ]
         },
@@ -41,19 +56,33 @@ function NavigationService() {
             "menuArray": [
                 {
                     "name": "Article Overview",
-                    "link": "articles\/list\/"
+                    "link": "articles\/list",
+                    "inMenu": true
                 },
                 {
                     "name": "Add Article",
-                    "link": "articles\/add\/"
+                    "link": "articles\/add",
+                    "inMenu": true
+                },
+                {
+                    "name": "Edit Article",
+                    "link": "articles\/article\/:id",
+                    "inMenu": false
                 },
                 {
                     "name": "Page Overview",
-                    "link": "pages\/list\/"
+                    "link": "pages\/list",
+                    "inMenu": true
                 },
                 {
                     "name": "Add Page",
-                    "link": "pages\/add\/"
+                    "link": "pages\/add",
+                    "inMenu": true
+                },
+                {
+                    "name": "Edit Page",
+                    "link": "pages\/page\/:id",
+                    "inMenu": false
                 }
             ]
         }
@@ -63,11 +92,11 @@ function NavigationService() {
         //    "menuArray": [
         //        {
         //            "name": "Übersicht",
-        //            "link": "#\/files\/"
+        //            "link": "#\/files"
         //        },
         //        {
         //            "name": "Upload",
-        //            "link": "#\/add-file\/"
+        //            "link": "#\/add-file"
         //        }
         //    ]
         //},
@@ -77,11 +106,11 @@ function NavigationService() {
         //    "menuArray": [
         //        {
         //            "name": "Übersicht",
-        //            "link": "#\/events\/"
+        //            "link": "#\/events"
         //        },
         //        {
         //            "name": "Event hinzufügen",
-        //            "link": "#\/add-event\/"
+        //            "link": "#\/add-event"
         //        },
         //        {
         //            "name": "Veranstaltungsort hinzufügen",
@@ -95,11 +124,11 @@ function NavigationService() {
         //    "menuArray": [
         //        {
         //            "name": "Übersicht",
-        //            "link": "#\/users\/"
+        //            "link": "#\/users"
         //        },
         //        {
         //            "name": "Benutzer hinzufügen",
-        //            "link": "#\/add-user\/"
+        //            "link": "#\/add-user"
         //        }
         //    ]
         //},
@@ -109,15 +138,15 @@ function NavigationService() {
         //    "menuArray": [
         //        {
         //            "name": "Übersicht",
-        //            "link": "#\/settings\/"
+        //            "link": "#\/settings"
         //        },
         //        {
         //            "name": "System Logging",
-        //            "link": "#\/settings\/system-logging\/"
+        //            "link": "#\/settings\/system-logging"
         //        },
         //        {
         //            "name": "Datenbank Logging",
-        //            "link": "#\/settings\/database-logging\/"
+        //            "link": "#\/settings\/database-logging"
         //        }
         //    ]
         //}
@@ -138,18 +167,32 @@ function NavigationService() {
     };
 
     /**
-     * Returns the subnavigation array of a given parent route.
+     * Returns the sub-navigation array of a given parent route.
      *
      * @param currentUrl The url of which to get the submenues
+     * @param isInMenuCheck Whether to return only entries which are inMenu
      * @returns { object }
      */
-    this.getSubnavigation = function (currentUrl) {
+    this.getSubNavigation = function (currentUrl, isInMenuCheck) {
         var routeElements = currentUrl.split("/");
+
         for (var property in routeElements) {
             if (routeElements.hasOwnProperty(property) && routeElements[property].length > 0) {
                 if (routeElements[property] in menuArray) {
-                    var subNav = {};
-                    subNav.menuArray = menuArray[routeElements[property]].menuArray;
+                    var subNav = {},
+                        entries = [];
+
+                    for (var key in menuArray[routeElements[property]].menuArray) {
+                        if (menuArray[routeElements[property]].menuArray.hasOwnProperty(key)) {
+                            if ((true === isInMenuCheck && true === menuArray[routeElements[property]].menuArray[key].inMenu) ||
+                                false === isInMenuCheck) {
+                                entries.push(menuArray[routeElements[property]].menuArray[key]);
+                            }
+
+                        }
+                    }
+
+                    subNav.menuArray = entries;
                     subNav.topBorderClass = menuArray[routeElements[property]].topBorderClass;
                     return subNav;
                 }
@@ -157,7 +200,61 @@ function NavigationService() {
         }
 
         return null;
-    }
+    };
+
+    /**
+     * Returns the name registered in the nav array above for a page
+     * on the given url
+     *
+     * @param currentUrl The url of the page of which to get its name
+     * @returns {string} A string representing the found name. Is empty if not found
+     */
+    this.getPageName = function (currentUrl) {
+        var subNav = that.getSubNavigation(currentUrl, false);
+
+        if (!('menuArray' in subNav)) {
+            return '';
+        }
+
+        // last two elements must match, e.g. 'article/add'
+        var routeElements = currentUrl.split("/");
+
+        if (routeElements.length < 2) {
+            // we can not handle this route
+            return '';
+        }
+
+        var route = routeElements[routeElements.length - 2] + "/" + routeElements[routeElements.length -1];
+
+        for (var entry in subNav.menuArray) {
+            if (subNav.menuArray.hasOwnProperty(entry) &&
+                subNav.menuArray[entry].hasOwnProperty('link') &&
+                subNav.menuArray[entry].hasOwnProperty('name')) {
+
+                if (route === subNav.menuArray[entry].link ) {
+                    // simple route
+                    return subNav.menuArray[entry].name;
+                }
+
+                if (subNav.menuArray[entry].link.indexOf(':id') === (subNav.menuArray[entry].link.length - 3)) {
+                    // link in menuArray ends with :id
+                    // does current route end with an id?
+                    var regEx = new RegExp("\\d+");
+                    var lastRouteElement = routeElements[routeElements.length - 1];
+                    var secondLastRouteElement = routeElements[routeElements.length - 2];
+                    var menuArrayRouteElements = subNav.menuArray[entry].link.split("/");
+
+                    if (true === regEx.test(lastRouteElement) &&
+                        menuArrayRouteElements[menuArrayRouteElements.length -2 ] === secondLastRouteElement) {
+                        return subNav.menuArray[entry].name;
+                    }
+                }
+
+            }
+        }
+
+        return '';
+    };
 
 }
 (function () {
