4a771dfb3eb62ca0c45b5931e09ef147fa6db794
Raphael Matile <raphael.matile@gmail.com>
2015-09-01 18:09:10 +0200

    Fix basic auth for php running on fpm/fast-cgi

--- web/index.php
+++ web/index.php
@@ -1,4 +1,25 @@
 <?php
+// fix basic auth headers for php running fpm/fast-cgi
+
+$authorizationHeader = null;
+if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    $authorizationHeader = $_SERVER['HTTP_AUTHORIZATION'];
+} elseif (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+    $authorizationHeader = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+}
+
+// Decode AUTHORIZATION header into PHP_AUTH_USER and PHP_AUTH_PW
+if (null !== $authorizationHeader) {
+    $exploded = explode(':', base64_decode(substr($authorizationHeader, 6)));
+    if (count($exploded) == 2) {
+        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = $exploded;
+    }
+}
+
+// PHP_AUTH_USER/PHP_AUTH_PW
+if (isset($_SERVER['PHP_AUTH_USER'])) {
+    $_SERVER['AUTHORIZATION'] = 'Basic '.base64_encode($_SERVER['PHP_AUTH_USER'].':'.$_SERVER['PHP_AUTH_PW']);
+}
 
 // constants, setup of slim-app
 require_once("../setup.php");
