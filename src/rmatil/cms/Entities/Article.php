509dcf9253b3ae8a1f80bfc61595adc7569a0da0
Raphael Matile <raphael.matile@gmail.com>
2015-09-06 21:10:23 +0200

    Readd use statement for annotations

--- src/rmatil/cms/Entities/Article.php
+++ src/rmatil/cms/Entities/Article.php
@@ -5,6 +5,7 @@ namespace rmatil\cms\Entities;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Type;
 
 /**
  * @ORM\Entity
--- src/rmatil/cms/Entities/Page.php
+++ src/rmatil/cms/Entities/Page.php
@@ -4,6 +4,7 @@ namespace rmatil\cms\Entities;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Type;
 
 /**
  * @ORM\Entity
