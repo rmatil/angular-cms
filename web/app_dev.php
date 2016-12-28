697bca03c2c266629b5f2fd183efb70b9aee156a
Raphael Matile <raphael.matile@gmail.com>
2016-12-28 10:28:49 +0100

    Disable classes.php file for debug mapping

--- web/app_dev.php
+++ web/app_dev.php
@@ -25,7 +25,8 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+// avoid using classes.php for debug mapping purposes
+//$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
